c4470dd33128e9e34cb74b8020c6f39505d0b7be
Wide White <54364666+chg-chg@users.noreply.github.com>
2024-08-04 04:12:26 +0800

    adcc

--- pytorch_grad_cam/metrics/ADCC.py
+++ pytorch_grad_cam/metrics/ADCC.py
@@ -1,3 +1,5 @@
+# https://github.com/aimagelab/ADCC/
+
 from scipy.linalg import norm
 from scipy import stats as STS
 import torch
@@ -10,13 +12,9 @@ import cv2
 def complexity(saliency_map):
     return abs(saliency_map).sum()/(saliency_map.shape[-1]*saliency_map.shape[-2])
 
-def coherency(saliency_map, explanation_map, arch, attr_method, out):
-    if torch.cuda.is_available():
-        explanation_map = explanation_map.cuda()
-        arch = arch.cuda()
+def coherency(saliency_map, explanation_map, attr_method, targets):
 
-    class_idx = out.max(1)[1].item()
-    saliency_map_B=attr_method(image=explanation_map, model=arch, classidx=class_idx)
+    saliency_map_B=attr_method(explanation_map, targets)
 
     A, B = saliency_map.detach(), saliency_map_B.detach()
 
@@ -35,19 +33,6 @@ def coherency(saliency_map, explanation_map, arch, attr_method, out):
         y = (y + 1) / 2
 
     return y,A,B
-def average_drop(image, explanation_map, arch, out, class_idx=None):
-
-    with torch.no_grad():
-        out_on_exp = FF.softmax(arch(explanation_map), dim=1)
-
-    confidence_on_inp = out.max(1)[0].item()
-
-    if class_idx is None:
-        class_idx = out.max(1)[1].item()
-
-    confidence_on_exp = out_on_exp[:,class_idx][0].item()
-
-    return max(0.,confidence_on_inp-confidence_on_exp)/confidence_on_inp
 
 
 
@@ -60,24 +45,20 @@ class ADCC:
                  targets: List[Callable],
                  model: torch.nn.Module,
                  cam_method,
-                 return_visualization=False,
-                 return_diff=True):
+                 return_visualization=False):
 
-        if return_diff:
-            with torch.no_grad():
-                outputs = model(input_tensor)
-                scores = [target(output).cpu().numpy()
-                          for target, output in zip(targets, outputs)]
-                scores = np.float32(scores)
+        with torch.no_grad():
+            outputs = model(input_tensor)
+            scores = [target(output).cpu().numpy()
+                      for target, output in zip(targets, outputs)]
+            scores = np.float32(scores)
 
-        batch_size = input_tensor.size(0)
         perturbated_tensors = []
-        for i in range(batch_size):
-            cam = cams[i]
-            tensor = self.perturbation(input_tensor[i, ...].cpu(),
-                                       torch.from_numpy(cam))
-            tensor = tensor.to(input_tensor.device)
-            perturbated_tensors.append(tensor.unsqueeze(0))
+        cam = cams[0]
+        tensor = self.perturbation(input_tensor[0, ...].cpu(),
+                                   torch.from_numpy(cam))
+        tensor = tensor.to(input_tensor.device)
+        perturbated_tensors.append(tensor.unsqueeze(0))
         perturbated_tensors = torch.cat(perturbated_tensors)
 
         with torch.no_grad():
@@ -87,13 +68,15 @@ class ADCC:
                 targets, outputs_after_imputation)]
         scores_after_imputation = np.float32(scores_after_imputation)
 
-        if return_diff:
-            result = scores_after_imputation - scores
-        else:
-            result = scores_after_imputation
+        result = scores_after_imputation - scores
+        avgdrop = max(0., scores - scores_after_imputation) / scores
+        com = complexity(cam)
+        coh,_,_ = coherency(cam, perturbated_tensors, cam_method, targets)
+
+        adcc = 3 / (1 / coh + 1 / (1 - com) + 1 / (1 - avgdrop))
 
         if return_visualization:
-            return result, perturbated_tensors
+            return adcc, perturbated_tensors
         else:
-            return result
+            return adcc
         
